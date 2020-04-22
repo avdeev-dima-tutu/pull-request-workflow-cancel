078a8a36f40e7b365f0c302c8337f188ddcb5842
avdim <avdim@mail.ru>
2020-04-22 23:17:16 +0300

    simplify param workflows

--- index.js
+++ index.js
@@ -8,51 +8,29 @@ const { Octokit } = require('@octokit/rest');
 const run = async () => {
     console.log("context", context);
     const { GITHUB_TOKEN } = process.env;
-    const workflows = core.getInput('workflows');
-    if (!workflows) {
-        core.setFailed('No `workflows` input given, aborting.');
-        return;
-    }
-
-    const names = workflows.split('|').map(m => m.trim());
-
+    const workflowName = context.workflow
     if (!GITHUB_TOKEN) {
         core.setFailed('GITHUB_TOKEN env variable is required...');
         return;
     }
-
     const client = new Octokit({ auth: `token ${GITHUB_TOKEN}` });
-
     const pullRequest = context.payload.pull_request;
-
     const { owner, repo } = context.repo;
+    console.log('Checking', workflowName, process.env.GITHUB_HEAD_REF);
+    const workflow = await client.actions.getWorkflow({
+        owner,
+        repo,
+        workflow_id: workflowName,
+    });
 
-    const runsAndWorkflows = await Promise.all(
-        names.map(async name => {
-            console.log('Checking', name, process.env.GITHUB_HEAD_REF);
-            try {
-                const workflow = await client.actions.getWorkflow({
-                    owner,
-                    repo,
-                    workflow_id: name,
-                });
-
-                const runs = await client.actions.listWorkflowRuns({
-                    owner,
-                    repo,
-                    workflow_id: workflow.data.id,
-                    branch: process.env.GITHUB_HEAD_REF,
-                    event: 'pull_request',
-                    status: 'in_progress',
-                });
-                return { workflow, runs };
-            } catch (err) {
-                console.error(err);
-                console.log('failed');
-                return null;
-            }
-        }),
-    );
+    const runs = await client.actions.listWorkflowRuns({
+        owner,
+        repo,
+        workflow_id: workflow.data.id,
+        branch: process.env.GITHUB_HEAD_REF,
+        event: 'pull_request',
+        status: 'in_progress',
+    });
     const latestPullRequestData = await client.pulls.get({
         owner,
         repo,
@@ -68,31 +46,29 @@ const run = async () => {
         );
         return;
     }
-    for (const { workflow, runs } of runsAndWorkflows.filter(Boolean)) {
-        for (const run of runs.data.workflow_runs) {
-            console.log('Currently running', run.id);
-            let samePr = false;
-            for (const pr of run.pull_requests) {
-                console.log(pr.url, pr.number);
-                if (pr.number === pullRequest.number) {
-                    samePr = true;
-                }
+    for (const run of runs.data.workflow_runs) {
+        console.log('Currently running', run.id);
+        let samePr = false;
+        for (const pr of run.pull_requests) {
+            console.log(pr.url, pr.number);
+            if (pr.number === pullRequest.number) {
+                samePr = true;
             }
+        }
 
-            // The run is behind!
-            if (samePr && run.head_sha !== pullRequest.head.sha) {
-                console.log(
-                    'Cancelling!',
-                    run.head_sha,
-                    process.env.GITHUB_SHA,
-                    pullRequest.head.sha,
-                );
-                await client.actions.cancelWorkflowRun({
-                    owner,
-                    repo,
-                    run_id: run.id,
-                });
-            }
+        // The run is behind!
+        if (samePr && run.head_sha !== pullRequest.head.sha) {
+            console.log(
+                'Cancelling!',
+                run.head_sha,
+                process.env.GITHUB_SHA,
+                pullRequest.head.sha,
+            );
+            await client.actions.cancelWorkflowRun({
+                owner,
+                repo,
+                run_id: run.id,
+            });
         }
     }
 };
